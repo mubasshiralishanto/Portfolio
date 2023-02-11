7e31f79a9d8a498d4b83d1452b0feac655c74f00
Mubasshir Ali <mubasshiralishanto@gmail.com>
2023-02-12 02:17:52 +0600

    Final Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text", {
+    strings: [  "Cinematographer",  "Photographer",
+    "Fullstack Developer"],
+    
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    // loop: true
+
+})
